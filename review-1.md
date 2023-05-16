e4337b83b15a70b6b09398337c3a55343f9e60fc
Jonathan <jonathan.e.bernal@gmail.com>
2023-05-15 20:51:50 -0500

    creacion de archivo readme

---
+++ review-1.md
@@ -0,0 +1,7 @@
+#¿Qué es mi producto y para que sirve?
+
+El servidor me crea una archivo tipo JSON en el cual el cliente puede visualizar el producto y mirar el id de la tarea la descripcion exacta de la tarea y por ultimo el estado de cada tarea para saber si esta completada o incompleta.
+
+#¿Cuáles son las funcionalidades más importantes y porque los usuarios las usarían?
+
+Las funciones mas importantes de este servidor es que cada cliente puede añadir tareas y el programa se actualiza dandole f5 o actualizando la pagina para visualizar cada tarea.
--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const host = "localhost";
 const port = 8080
 
 const tareas = [
-  { id: 1, descripcion: "lavar", completado: false},
-  { id: 2, descripcion: "cocinar", completado: false },
-  { id: 3, descripcion: "planchar", completado: false }
+  { id: 1, description: "lavar", state: false},
+  { id: 2, description: "cocinar", state: false },
+  { id: 3, description: "planchar", state: false },
+  { id: 4, description: "lavar platos", state: false },
+  { id: 5, description: "barrer", state: false }
+
 ];
 
-const server = http.createServer((req, res) =>{   
+const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify(tareas))
   res.end();
